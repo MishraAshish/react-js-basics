d282393eb2121bff6ab12fa8d5bb7100ca49a6ea
Ashish <ash.ishu025g@gmail.com>
2018-07-08 23:25:26 +0530

    upto two way binding

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,12 +1,39 @@
 import React from "react";
 
 export class Home extends React.Component{
+    constructor(props){
+        super();
+        this.state = {
+            age : props.user.age,
+            status:0,
+            homeLink:props.initialLink
+        }
+    }
+
+    makeMeOlder(){
+        this.setState({
+            age : this.state.age + 3
+        });      
+    }
+
+    changeLink(){
+        this.props.changeLink(this.state.homeLink);
+    }
+
+    onHandleChange(event){
+        this.setState(
+        {
+            homeLink:event.target.value
+        });
+    }
+
     render(){
         return(
-            
-                <div className="container">
+            <div className="container">
                     <p>In a new Component</p>
                     <p>Your name is {this.props.name} </p>
+                    <p>Your age is {this.state.age} </p>
+                    <p>Your age is {this.state.status} </p>
                     <div>
                         <h4>Hobbies</h4>
                         <ul>
@@ -15,12 +42,21 @@ export class Home extends React.Component{
                     </div>
                     <hr/>           
                     {this.props.children}
-                </div>
-            
+                    <hr/>
+                    <button className="btn btn-primary" onClick={() => this.makeMeOlder()}>Click To Old</button>
+                    <hr/>
+                    <button className="btn btn-primary" onClick={this.props.greet}>Greet Me</button>
+                    <hr/>
+                    <input type="text" value={this.state.homeLink} 
+                        onChange={(event) => this.onHandleChange(event)}/>
+                    <button className="btn btn-primary" onClick={this.changeLink.bind(this)}>Change Link</button>
+            </div>
         );
     }
 }
 Home.propTypes = {
     //name: React.PropTypes.string,
     //user: React.PropTypes.object 
+    //https://www.youtube.com/watch?v=IK9k9hSuYeA&index=14&list=PL55RiY5tL51oyA8euSROLjMFZbXaV7skS
+    //UpTo : #13 :Two Way Binding Need to start #14
 }
--- src/app/index.js
+++ src/app/index.js
@@ -5,21 +5,41 @@ import { Header } from "./components/Header";
 import { Home } from "./components/Home";
 
 class App extends React.Component{
+    constructor(){
+        super();
+        this.state={
+            homeLink:"Home"
+        }
+    };
+    onGreet(){
+        alert("Hello");
+    };
+    changeLink(newLink){
+        this.setState({
+            homeLink : newLink
+        })
+    }
     render(){
         var user = {
             age:25,
             hobbies:["PratapVihar","Cricker"]
-        };
+        };        
         return(
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Header/>
+                        <Header headerLink={this.state.homeLink}/>
                     </div>                    
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home name={"Ashish"} user={user}>
+                        <Home 
+                            name={"Ashish"} 
+                            user={user}                             
+                            greet={this.onGreet}
+                            changeLink = {this.changeLink.bind(this)}
+                            initialLink = {this.state.homeLink}
+                            >
                             <p>This is a child Component</p>
                         </Home>                        
                     </div>                    
